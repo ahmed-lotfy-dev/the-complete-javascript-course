2f71dcdc1e4efa4df22af6e6e3d704d860a68c4d
Ahmed Lotfy <elshenawy19@gmail.com>
2022-05-13 15:53:21 +0200

    data types lecture

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+let country = 'Egypt';
+let continent = 'Africa';
+let population = 1340598147;
+
+console.log(country, continent, population);
+
+let isIsland = false;
+let language;
+
+console.log(country, continent, population, isIsland, language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+/*
+// Linking a JavaScript File
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+// Values and Variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+console.log(firstName);
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+*/
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Ahmed');
