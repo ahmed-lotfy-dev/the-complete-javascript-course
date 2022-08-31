a1ac22be132a6f0627c7161a596c76f8494893a5
alotfy2019 <elshenawy19@gmail.com>
2022-08-31 17:54:36 +0200

    Finished Search Result Lectures 297

---
+++ 18-forkify/starter/src/js/model.js
@@ -0,0 +1,54 @@
+import { API_URL, KEY } from './config';
+import { getJson } from './helpers';
+import recipeView from './views/recipeView';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJson(`${API_URL}${id}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    // console.log(state.recipe);
+  } catch (error) {
+    //Temp Error Handling
+    console.error(`${error} 💥💥💥💥💥💥`);
+    throw error;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJson(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (error) {
+    console.error(`${error} 💥💥💥💥💥💥`);
+    throw error;
+  }
+};
